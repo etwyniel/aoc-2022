4e05b9938337c7cca8f700ff1812c4eb55ca324e
Aymeric Beringer <aymericberin@gmail.com>
2022-12-23 16:26:10 +0100

    Faster day 23

--- src/day23.rs
+++ src/day23.rs
@@ -20,92 +20,120 @@ impl_day!(Day23::{Part1, Part2}: 2022[23], r"
 .#..#..
 ");
 
-fn display(points: &FxHashSet<Point>) {
-    let mut minx = isize::MAX;
-    let mut maxx = isize::MIN;
-    let mut miny = isize::MAX;
-    let mut maxy = isize::MIN;
-    points.iter().for_each(|&Point { x, y }| {
-        minx = minx.min(x);
-        maxx = maxx.max(x);
-        miny = miny.min(y);
-        maxy = maxy.max(y);
-    });
+struct State {
+    points: FxHashSet<Point>,
+    directions: VecDeque<Direction>,
+    intents: Vec<(Point, Point)>,
+    counts: FxHashMap<Point, u8>,
+}
 
-    miny = miny.min(-1);
-    minx = minx.min(-3);
-    for y in miny..=maxy {
-        for x in minx..=maxx {
-            if points.contains(&Point { x, y }) {
-                eprint!("#");
-            } else {
-                eprint!(".");
+impl State {
+    fn simulate(&mut self) -> bool {
+        let mut moved = false;
+        self.intents.clear();
+        self.counts.clear();
+        // Avoid iterating on VecDeque in hot loop
+        let directions: [Direction; 4] = [
+            self.directions[0],
+            self.directions[1],
+            self.directions[2],
+            self.directions[3],
+        ];
+        for &p in self.points.iter() {
+            let mut neighbors = [false; 3 * 3];
+            let mut count = 0;
+            neighbors.iter_mut().enumerate().for_each(|(i, b)| {
+                *b = i != 4
+                    && self.points.contains(&Point {
+                        x: p.x + (i % 3 - 1) as isize,
+                        y: p.y + (i / 3 - 1) as isize,
+                    });
+                if *b {
+                    count += 1;
+                }
+            });
+            if count == 0 {
+                // no neighbors, no move
+                continue;
             }
+            let intent = directions
+                .into_iter()
+                .find(|&dir| {
+                    ((dir as i8 - 1)..=(dir as i8 + 1))
+                        .map(|d| (dir.delta() | Direction::from(d).delta()) + Point { x: 1, y: 1 })
+                        .all(|dest| !neighbors[dest.offset(3)])
+                })
+                .map(|dir| p + dir.delta());
+            if let Some(dest) = intent {
+                self.intents.push((p, dest));
+                *self.counts.entry(dest).or_default() += 1;
+            }
+        }
+        for &(p, dest) in &self.intents {
+            if self.counts[&dest] > 1 {
+                continue;
+            }
+            self.points.remove(&p);
+            self.points.insert(dest);
+            moved = true;
+        }
+        // self.display();
+        self.directions.rotate_left(1);
+        moved
+    }
+
+    fn new(input: impl Iterator<Item = String>) -> Self {
+        let mut points: FxHashSet<Point> = FxHashSet::default();
+        input.enumerate().for_each(|(y, ln)| {
+            ln.bytes()
+                .enumerate()
+                .filter(|(_, b)| *b == b'#')
+                .for_each(|(x, _)| {
+                    points.insert((x, y).into());
+                })
+        });
+        let mut directions = VecDeque::with_capacity(4);
+        directions.push_back(Up);
+        directions.push_back(Down);
+        directions.push_back(Left);
+        directions.push_back(Right);
+        let intents = Vec::with_capacity(points.len());
+        let mut counts = FxHashMap::default();
+        counts.reserve(points.len());
+        State {
+            points,
+            directions,
+            intents,
+            counts,
+        }
+    }
+
+    fn display(&self) {
+        let mut minx = isize::MAX;
+        let mut maxx = isize::MIN;
+        let mut miny = isize::MAX;
+        let mut maxy = isize::MIN;
+        self.points.iter().for_each(|&Point { x, y }| {
+            minx = minx.min(x);
+            maxx = maxx.max(x);
+            miny = miny.min(y);
+            maxy = maxy.max(y);
+        });
+
+        miny = miny.min(-1);
+        minx = minx.min(-3);
+        for y in miny..=maxy {
+            for x in minx..=maxx {
+                if self.points.contains(&Point { x, y }) {
+                    eprint!("#");
+                } else {
+                    eprint!(".");
+                }
+            }
+            eprintln!();
         }
         eprintln!();
     }
-    eprintln!();
-}
-
-fn simulate(points: &mut FxHashSet<Point>, directions: &mut VecDeque<Direction>) -> bool {
-    let mut moved = false;
-    let mut intents = FxHashMap::<Point, Point>::default();
-    let mut counts = FxHashMap::<Point, u8>::default();
-    intents.reserve(points.len());
-    counts.reserve(points.len());
-    for &p in points.iter() {
-        let neighbors = (-1..=1)
-            .flat_map(|y| (-1..=1).map(move |x| (x + p.x, y + p.y)))
-            .map(|(x, y)| points.contains(&Point { x, y }))
-            .collect_vec();
-        if neighbors.iter().copied().filter(|&b| b).count() <= 1 {
-            // no neighbors, no move
-            continue;
-        }
-        let intent = directions
-            .iter()
-            .find(|&&dir| {
-                ((dir as i8 - 1)..=(dir as i8 + 1))
-                    .map(|d| {
-                        (dir.delta() + Direction::from(d).delta()).signum() + Point { x: 1, y: 1 }
-                    })
-                    .all(|dest| !neighbors[dest.offset(3)])
-            })
-            .map(|dir| p + dir.delta());
-        if let Some(dest) = intent {
-            intents.insert(p, dest);
-            *counts.entry(dest).or_default() += 1;
-        }
-    }
-    for (p, dest) in intents {
-        if counts[&dest] > 1 {
-            continue;
-        }
-        points.remove(&p);
-        points.insert(dest);
-        moved = true;
-    }
-    // display(&points);
-    directions.rotate_left(1);
-    moved
-}
-
-fn init(input: impl Iterator<Item = String>) -> (FxHashSet<Point>, VecDeque<Direction>) {
-    let mut points: FxHashSet<Point> = FxHashSet::default();
-    input.enumerate().for_each(|(y, ln)| {
-        ln.bytes()
-            .enumerate()
-            .filter(|(_, b)| *b == b'#')
-            .for_each(|(x, _)| {
-                points.insert((x, y).into());
-            })
-    });
-    let mut directions = VecDeque::with_capacity(4);
-    directions.push_back(Up);
-    directions.push_back(Down);
-    directions.push_back(Left);
-    directions.push_back(Right);
-    (points, directions)
 }
 
 pub struct Part1;
@@ -116,22 +144,22 @@ impl Part for Part1 {
     const EXAMPLE_RESULT: Option<Answer> = Some(Num(110));
 
     fn run(input: impl Iterator<Item = String>) -> anyhow::Result<Answer> {
-        let (mut points, mut directions) = init(input);
+        let mut state = State::new(input);
         // display(&points);
         for _ in 0..10 {
-            simulate(&mut points, &mut directions);
+            state.simulate();
         }
         let mut minx = isize::MAX;
         let mut maxx = isize::MIN;
         let mut miny = isize::MAX;
         let mut maxy = isize::MIN;
-        points.iter().for_each(|&Point { x, y }| {
+        state.points.iter().for_each(|&Point { x, y }| {
             minx = minx.min(x);
             maxx = maxx.max(x);
             miny = miny.min(y);
             maxy = maxy.max(y);
         });
-        let total = ((maxx - minx + 1) * (maxy - miny + 1)) as usize - points.len();
+        let total = ((maxx - minx + 1) * (maxy - miny + 1)) as usize - state.points.len();
         Ok(Num(total as u64))
     }
 }
@@ -144,11 +172,9 @@ impl Part for Part2 {
     const EXAMPLE_RESULT: Option<Answer> = Some(Num(20));
 
     fn run(input: impl Iterator<Item = String>) -> anyhow::Result<Answer> {
-        let (mut points, mut directions) = init(input);
+        let mut state = State::new(input);
         // display(&points);
-        let res = (1..)
-            .find(|_| !simulate(&mut points, &mut directions))
-            .unwrap_or_default();
+        let res = (1..).find(|_| !state.simulate()).unwrap_or_default();
         Ok(Num(res))
     }
 }
